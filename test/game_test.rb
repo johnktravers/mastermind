77b6828912cc70910b79dc1691a2d09d3d76c2a7
John Travers <johnk.travers@gmail.com>
2019-10-18 09:33:52 -0600

    Add test that game initializes without turns and a new combo

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/game'
 
 class GameTest < Minitest::Test
@@ -12,9 +13,24 @@ class GameTest < Minitest::Test
     assert_instance_of Game, @game
   end
 
+  def test_initialize
+    assert_equal 4, @game.combo.length
+    assert_equal [], @game.turns
+  end
+
+  def test_new_combo
+    assert_equal true, @game.new_combo.match?(/[rybg]/)
+    assert_equal 4, @game.new_combo.length
+  end
+
   def test_start
     expected = "Welcome to MASTERMIND\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?\n> "
     assert_equal expected, @game.start
+  end
+
+  def test_play
+    expected = "I have generated a beginner sequence with four elements made up of: (r)ed, (g)reen, (b)lue, and (y)ellow.\nUse (q)uit at any time to end the game.\n\nWhat's your guess?\n> "
+    assert_equal expected, @game.play
   end
 
 
